e549091119f252403ed402de8ce9ee5c042b883b
Fred Cons <cons@fullsix.com>
2013-10-25 15:24:53 +0200

    newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['technoshare-logs'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'c8a51997ca33a1743ca677daae930a0113f3d495',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
